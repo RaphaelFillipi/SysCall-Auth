1a1caa045f8ad1650c671aa3fbac8617d4668304
karolinyB <ana.silva@baymetrics.com.br>
2025-06-15 20:45:59 -0300

    feat: add RegisterUser component

---
+++ src/root.component.tsx
@@ -0,0 +1,10 @@
+//import "./frontend/style/index.css";
+import { RegisterUser } from "./frontend/pages/RegisterUser";
+
+export default function Root(props) {
+  return (
+    <div className="h-screen">
+      <RegisterUser />
+    </div>
+  );
+}
