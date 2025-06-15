619e55e095457d492b56c31395947bbf542382a6
karolinyB <ana.silva@baymetrics.com.br>
2025-06-15 20:49:27 -0300

    feat: create FieldError component to display Zod validation errors

---
+++ src/frontend/components/Form/FieldError.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FieldErrorProps } from "../../types/validators/FieldErrorProps.type";
+
+export const FieldError: React.FC<FieldErrorProps> = ({ error }) => {
+  if (!error) return null;
+
+  const messages = error.types ? Object.values(error.types) : [error.message];
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', marginTop: '4px',  whiteSpace: "pre-line" }}>
+      {messages.map((msg, idx) => (
+        <span key={idx}>{msg}</span>
+      ))}
+    </div>
+  );
+};
