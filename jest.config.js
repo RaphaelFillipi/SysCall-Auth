0a3d948120a1b9602f2fb33b07673876661a74dd
karolinyB <ana.silva@baymetrics.com.br>
2025-06-15 20:21:48 -0300

    chore: initial infrastructure and single-spa setup

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rootDir: "src",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(j|t)sx?$": "babel-jest",
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(@hookform|zod)/)",
+  ],
+  moduleNameMapper: {
+    "\\.(css)$": "identity-obj-proxy",
+    "single-spa-react/parcel": "single-spa-react/lib/cjs/parcel.cjs",
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+};
