6f6bad8f2838e3ad67f295b2bc0444f85f1b2595
karolinyB <ana.silva@baymetrics.com.br>
2025-06-16 19:40:00 -0300

    feat: create App component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { LoginUser } from "./frontend/pages/LoginUser";
+import { RegisterUser } from "./frontend/pages/RegisterUser";
+
+export function App() {
+  return (
+    <div className="h-screen">
+      <RegisterUser />
+      {/* <LoginUser /> */}
+    </div>
+  );
+}
