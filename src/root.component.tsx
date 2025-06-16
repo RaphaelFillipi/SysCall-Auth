5ec992cf6944e2fa4400694d5cb61b758a0d97b5
karolinyB <ana.silva@baymetrics.com.br>
2025-06-16 02:29:41 -0300

    feat: add Nhost and Apollo Client providers

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,10 +1,20 @@
 //import "./frontend/style/index.css";
 import { RegisterUser } from "./frontend/pages/RegisterUser";
+import { NhostProvider } from "@nhost/react";
+import { ApolloProvider } from "@apollo/client";
+import { nhost } from "./lib/nhost/nhost";
+import { apolloClient } from "./lib/apolloClient/apolloClient";
+import { LoginUser } from "./frontend/pages/LoginUser";
 
 export default function Root(props) {
   return (
-    <div className="h-screen">
-      <RegisterUser />
-    </div>
+    <NhostProvider nhost={nhost}>
+      <ApolloProvider client={apolloClient}>
+        <div className="h-screen">
+          <RegisterUser />
+          {/* <LoginUser /> */}
+        </div>
+      </ApolloProvider>
+    </NhostProvider>
   );
 }
