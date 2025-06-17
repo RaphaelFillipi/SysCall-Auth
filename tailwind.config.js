bff48c6d2a11916b9d5badc8a6976a65581d2ea4
karolinyB <ana.silva@baymetrics.com.br>
2025-06-17 19:18:55 -0300

    chore: add new custom color to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       colors: {
         "green/light": "#E3F5E3",
         "green/dark": "#358438",
+        "gray/light": "#F0F4F8",
+      },
+      fontFamily: {
+        sans: ["Nunito", "sans-serif"],
       },
     },
   },
