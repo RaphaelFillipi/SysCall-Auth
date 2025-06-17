37064e8e3eb8d970ca563b2648424d6dd756cce6
karolinyB <ana.silva@baymetrics.com.br>
2025-06-17 19:37:34 -0300

    feat: create ButtonTitleAuth component

---
+++ src/frontend/components/Form/ButtonTitle/ButtonTitleAuth.tsx
@@ -0,0 +1,16 @@
+import { ButtonTitleAuthProps } from "@/frontend/types/forms/ButtonTitleAuthProps.type";
+
+export function ButtonTitleAuth({ title, active, className, ...rest }) {
+  return (
+    <div className="w-1/2">
+      <button
+        className={`text-[22px] h-[46px] w-full transition-colors duration-200 ${className} ${
+          active ? "bg-transparent" : "bg-gray/light"
+        }`}
+        {...rest}
+      >
+        {title}
+      </button>
+    </div>
+  );
+}
