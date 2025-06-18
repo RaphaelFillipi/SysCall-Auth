26e30688ee29d26818a6ac9fa9f47cc77423688e
karolinyB <ana.silva@baymetrics.com.br>
2025-06-18 19:31:15 -0300

    style: adjust colors of components and its elements

--- src/frontend/components/StandardButton.tsx
+++ src/frontend/components/StandardButton.tsx
@@ -1,7 +1,7 @@
 export function StandardButton({ title }) {
   return (
     <div>
-      <button className="h-[36px] md:h-[59px] w-full bg-green/dark rounded-[8px] text-white font-bold">
+      <button className="h-[36px] md:h-[59px] w-full bg-green-dark rounded-[8px] text-white-primary font-bold">
         {title}
       </button>
     </div>
--- src/frontend/pages/SwitchFormButton.tsx
+++ src/frontend/pages/SwitchFormButton.tsx
@@ -2,14 +2,13 @@ import { useState } from "react";
 import { ButtonTitleAuth } from "../components/Form/ButtonTitle/ButtonTitleAuth";
 import { RegisterUser } from "./RegisterUser";
 import { LoginUser } from "./LoginUser";
-import { boolean } from "zod";
 
 export function SwitchFormButton() {
   const [formState, setFormState] = useState(true);
 
   return (
     <div className="w-full p-4 flex justify-center">
-      <div className="bg-green/light w-full text-sm md:text-2xl md:max-w-[591px] rounded-[12px] overflow-hidden h-full">
+      <div className="bg-green-light w-full text-sm md:text-2xl md:max-w-[591px] rounded-[12px] overflow-hidden h-full">
         <div className="flex flex-row justify-around w-full h-full pb-8">
           <ButtonTitleAuth
             title="Cadastro"
