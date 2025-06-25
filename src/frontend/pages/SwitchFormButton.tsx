1685ffa5d04d02f1fffad2a3d2e8748d9e92996d
karolinyB <ana.silva@baymetrics.com.br>
2025-06-25 12:44:46 -0300

    feat: include logo and decorative illustration in authentication screen

--- src/frontend/pages/SwitchFormButton.tsx
+++ src/frontend/pages/SwitchFormButton.tsx
@@ -2,33 +2,50 @@ import { useState } from "react";
 import { ButtonTitleAuth } from "../components/Form/ButtonTitle/ButtonTitleAuth";
 import { RegisterUser } from "./RegisterUser";
 import { LoginUser } from "./LoginUser";
+import logoVertical from "../img/logo-vertical.svg";
+import imgBg from "../img/image-bg.svg";
 
 export function SwitchFormButton() {
-  const [formState, setFormState] = useState(true);
+  const [formState, setFormState] = useState(false);
 
   return (
-    <div className="w-full p-4 flex justify-center">
-      <div className="bg-green-light w-full text-sm md:text-2xl md:max-w-[591px] rounded-[12px] overflow-hidden h-full">
-        <div className="flex flex-row justify-around w-full h-full pb-8">
-          <ButtonTitleAuth
-            title="Cadastro"
-            onClick={() => {
-              setFormState(false);
-            }}
-            active={!formState}
-            className={"rounded-br-lg"}
-          />
-          <ButtonTitleAuth
-            title="Login"
-            onClick={() => {
-              setFormState(true);
-            }}
-            active={formState}
-            className={"rounded-bl-lg"}
-          />
+    <div className="w-full min-h-screen flex flex-col md:flex-row">
+      <div className="w-full h-auto p-6 flex justify-center items-start sm:justify-center md:w-auto">
+        <img
+          src={logoVertical}
+          alt="Logo SysCall"
+          className="max-h-[95px] max-w-[90px] lg:max-h-[75px] lg:max-w-[70px]"
+        />
+      </div>
+      <div className="flex px-8 pb-4 justify-center items-center flex-grow lg:justify-start md:p-4 2xl:justify-center">
+        <div className="bg-green-light w-full sm:max-w-[480px] md:max-w-[400px] lg:max-w-[360px] xl:max-w-[400px] rounded-[12px]">
+          <div className="flex flex-row w-full pb-8 rounded-[12px] overflow-hidden lg:pb-4">
+            <ButtonTitleAuth
+              title="Cadastro"
+              onClick={() => {
+                setFormState(false);
+              }}
+              active={!formState}
+              className={"rounded-br-lg"}
+            />
+            <ButtonTitleAuth
+              title="Login"
+              onClick={() => {
+                setFormState(true);
+              }}
+              active={formState}
+              className={"rounded-bl-lg"}
+            />
+          </div>
+          {formState === true ? <LoginUser /> : <RegisterUser />}
         </div>
-        {formState === true ? <LoginUser /> : <RegisterUser />}
-        {/* <LoginUser /> */}
+      </div>
+      <div className="lg:w-1/2 lg:flex lg:items-start lg:justify-center lg:max-h-screen lg:px-10 lg:pt-[35px] xl:items-center xl:pt-[0px]">
+        <img
+          src={imgBg}
+          alt="Pessoas"
+          className="h-[0px] w-[0px] lg:w-full lg:h-auto xl:w-full xl:max-h-screen"
+        />
       </div>
     </div>
   );
