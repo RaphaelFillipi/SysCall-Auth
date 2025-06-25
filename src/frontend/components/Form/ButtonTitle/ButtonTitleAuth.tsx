5649576cdf008451a712c8398dad4719849820ac
karolinyB <ana.silva@baymetrics.com.br>
2025-06-25 12:41:04 -0300

    style: update styling of Button component

--- src/frontend/components/Form/ButtonTitle/ButtonTitleAuth.tsx
+++ src/frontend/components/Form/ButtonTitle/ButtonTitleAuth.tsx
@@ -2,7 +2,7 @@ export function ButtonTitleAuth({ title, active, className, ...rest }) {
   return (
     <div className="w-1/2">
       <button
-        className={`text-[22px] h-[46px] w-full transition-colors duration-200 ${className} ${
+        className={`text-[22px] p-2 w-full transition-colors duration-200 font-bold ${className} md:text-[24px]  ${
           active ? "bg-transparent" : "bg-gray-light"
         }`}
         {...rest}
