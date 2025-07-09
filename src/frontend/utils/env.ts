5637ee08dcb2ccd794051abbb29e281708a53ca3
karolinyB <ana.silva@baymetrics.com.br>
2025-07-09 18:56:41 -0300

    feat: add constants for connecting to Nhost and Apollo Client

---
+++ src/frontend/utils/env.ts
@@ -0,0 +1,11 @@
+// Variáveis de Ambiente
+export const isDevEnv = () => process.env.NODE_ENV === "development";
+export const isProdEnv = () => process.env.NODE_ENV === "production";
+
+// Conexão com o Nhost
+export const subdomain: string = "sskqrcxhoncbtbqmcxvr";
+export const region: string = "sa-east-1";
+
+// Headers
+export const role: string = "usr";
+export const admin_secret: string = "$%-oTb61U)!HpStRi+%CRvGb6wZhkaY^";
