31ef7808c8bec13d0dd90374ff4b548cef0893c9
karolinyB <ana.silva@baymetrics.com.br>
2025-07-10 00:00:25 -0300

    refactor: add try-catch to login logic

--- src/frontend/hooks/useLoginSubmit.ts
+++ src/frontend/hooks/useLoginSubmit.ts
@@ -1,17 +1,21 @@
 import { auth } from "../services/login/auth";
-import { error, log } from "../../utils/logger";
+import { error, log } from "../utils/logger";
 import { token } from "../services/login/token";
 import { DataLogin } from "../types/login/DataLogin.type";
 
 export function useLoginSubmit() {
   const handleLogin = async (data: DataLogin) => {
-    const result = await auth(data);
+    try {
+      const result = await auth(data);
 
-    if (result.error) {
-      error("Erro ao logar:", result.error.message);
-    } else {
-      log("Usuário logado com sucesso!");
-      log("JWT Token: ", token());
+      if (result.error) {
+        error("Erro ao logar:", result.error.message);
+      } else {
+        log("Usuário logado com sucesso!");
+        log("JWT Token: ", token());
+      }
+    } catch (error) {
+      error("Não foi possível retornar os dados da autenticação");
     }
   };
 
