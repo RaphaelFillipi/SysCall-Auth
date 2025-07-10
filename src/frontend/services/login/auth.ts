95bf24712d999f6178fef80ba8730c0448114b68
karolinyB <ana.silva@baymetrics.com.br>
2025-07-10 00:23:16 -0300

    refactor: add try-catch for authentication function with Nhost

--- src/frontend/services/login/auth.ts
+++ src/frontend/services/login/auth.ts
@@ -1,10 +1,17 @@
 import { DataLogin } from "@/frontend/types/login/DataLogin.type";
 import { SignInResponse } from "@nhost/nhost-js";
 import { nhost } from "../../../lib/nhost/nhost";
+import { error } from "../../utils/logger";
 
 export const auth = async (data: DataLogin): Promise<SignInResponse> => {
-  return await nhost.auth.signIn({
-    email: data.email,
-    password: data.password,
-  });
+  try {
+    const result = await nhost.auth.signIn({
+      email: data.email,
+      password: data.password,
+    });
+
+    return result;
+  } catch (e) {
+    error("Não foi possível efetuar a autenticação");
+  }
 };
